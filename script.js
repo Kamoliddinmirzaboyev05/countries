cb181d5709b743a37b2539e1016a478c3c76b765
Kamoliddin Mirzaboyev <kamoliddinmirzaboyev05@gmail.com>
2025-01-23 11:52:26 +0500

    Update

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-
+document.addEventListener("DOMContentLoaded", () => {
   const body = document.querySelector("body");
   const mode = document.querySelector(".mode");
   const btnImg = document.querySelector(".btnImg");
+  const block = document.querySelector(".block");
+
   var darkMode;
   const changeMode = () => {
     darkMode = localStorage.getItem("darkMode")
@@ -22,7 +24,45 @@
     } else {
       localStorage.setItem("darkMode", "true");
     }
-    console.log(darkMode);
     changeMode();
   });
 
+  // Starting api
+  let data;
+  const apiLink = "https://restcountries.com/v3.1/all";
+const writeData = (DB)=>{
+    DB.forEach((item)=>{
+        block.innerHTML += `
+        <div class="card">
+              <div class="flag">
+                <img src="${item.flags.png}" alt="" />
+              </div>
+              <div class="card-text">
+                <h2 class="country-name">${item.name.common}</h2>
+                <div class="row">
+                  <p class="key">Population:</p>
+                  <p class="value">81.770.900</p>
+                </div>
+                <div class="row">
+                  <p class="key">Region:</p>
+                  <p class="value">Europe</p>
+                </div>
+                <div class="row">
+                  <p class="key">Capital:</p>
+                  <p class="value">${item.capital}</p>
+                </div>
+              </div>
+            </div>
+        `
+    })
+}
+  const getData = async (link) => {
+    const req = await fetch(link);
+     data = await req.json();
+     writeData(data)
+     console.log(data);
+     
+  };
+  getData(apiLink);
+  
+});
