11263991731be845f42c4bab5dd92a52b0b13b59
Kamoliddin Mirzaboyev <kamoliddinmirzaboyev05@gmail.com>
2025-01-26 11:10:11 +0500

    Update

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
+const loader = document.querySelector(".loader");
 document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    loader.style.opacity = "0";
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 500);
+  }, 1000);
   const body = document.querySelector("body");
-  const mode = document.querySelector(".mode");
+  const mode = document.querySelectorAll(".mode");
   const btnImg = document.querySelector(".btnImg");
   const block = document.querySelector(".block");
+  const backBtn = document.querySelector(".backBtn");
+  const modal = document.querySelector(".modal");
   const searchInput = document.querySelector(".searchInput");
-  var word;
-  var darkMode;
+  const select = document.querySelector("select");
+  const modalData = document.querySelector(".modal-data");
+
   const changeMode = () => {
     darkMode = localStorage.getItem("darkMode")
       ? localStorage.getItem("darkMode")
@@ -18,14 +28,19 @@ document.addEventListener("DOMContentLoaded", () => {
       btnImg.setAttribute("src", "img/moon.svg");
     }
   };
+  backBtn.addEventListener("click", () => {
+    modal.style.bottom = "-1000px";
+  });
   changeMode();
-  mode.addEventListener("click", () => {
-    if (body.classList.contains("dark")) {
-      localStorage.setItem("darkMode", "false");
-    } else {
-      localStorage.setItem("darkMode", "true");
-    }
-    changeMode();
+  mode.forEach((modeBtn) => {
+    modeBtn.addEventListener("click", () => {
+      if (body.classList.contains("dark")) {
+        localStorage.setItem("darkMode", "false");
+      } else {
+        localStorage.setItem("darkMode", "true");
+      }
+      changeMode();
+    });
   });
 
   // Starting api
@@ -84,7 +99,7 @@ document.addEventListener("DOMContentLoaded", () => {
       block.innerHTML += `
         <div class="card">
               <div class="flag">
-                <img src="${item.flags.png}" alt="" />
+                <img class="flagImg" src="${item.flags.png}" alt="" />
               </div>
               <div class="card-text">
                 <h2 class="country-name">${item.name.common}</h2>
@@ -93,8 +108,8 @@ document.addEventListener("DOMContentLoaded", () => {
                   <p class="value">${population}</p>
                 </div>
                 <div class="row">
-                  <p class="key">Region:</p>
-                  <p class="value">${item.region}</p>
+                  <p class="key ">Region:</p>
+                  <p class="value region">${item.region}</p>
                 </div>
                 <div class="row">
                   <p class="key">Capital:</p>
@@ -105,19 +120,128 @@ document.addEventListener("DOMContentLoaded", () => {
         `;
     });
   };
-  const searchData = (DB, word) => {
-    var searched = DB.filter((item) => {
-      return item.name.common.includes(word);
-    });
-  };
 
   //   Synxron funksiya start
   const getData = async (link) => {
     const req = await fetch(link);
     data = await req.json();
     writeData(data);
-    console.log(data[27].population);
+    writeModal(data);
+    console.log(data);
   };
   getData(apiLink);
   // Synxron funksiya end
+
+  //search uchun yozilgan funksiya
+
+  searchInput.addEventListener("input", () => {
+    const cards = block.querySelectorAll(".card");
+
+    cards.forEach((card) => {
+      var title = card.querySelector("h2").textContent.toLowerCase();
+
+      if (!title.includes(searchInput.value.toLowerCase())) {
+        card.classList.add("hidden");
+      } else {
+        card.classList.remove("hidden");
+      }
+    });
+  });
+
+  // filter cuhun yozilgan funksiya
+
+  select.addEventListener("change", () => {
+    const cards = block.querySelectorAll(".card");
+    cards.forEach((card) => {
+      var region = card.querySelector(".region").textContent.toLowerCase();
+      if (!region.includes(select.value.toLowerCase())) {
+        card.classList.add("hidden");
+      } else {
+        card.classList.remove("hidden");
+      }
+
+      if (select.value == "all") {
+        card.classList.remove("hidden");
+      }
+    });
+  });
+
+  //modal uchun yzoilgan funksiya
+  let country, clicked;
+  window.addEventListener("click", (e) => {
+    if (e.target.parentElement.parentElement.classList.contains("card")) {
+      country =
+        e.target.parentElement.parentElement.querySelector("h2").textContent;
+      clicked = e.target.parentElement.parentElement.classList.contains("card");
+    }
+  });
+  const writeModal = (DB) => {
+    DB.forEach((item) => {
+      window.addEventListener("click", () => {
+        if (item.name.common == country) {
+          if (clicked) {
+            modal.style.top = "0px";
+          }
+          const currencyKeys = Object.values(item.currencies);
+          const languagesKeys = Object.values(item.languages);
+          languagesKeys.forEach((lang) => {});
+
+          modalData.innerHTML = `
+          <div class="modal-img">
+                <img src="${item.flags.png}" alt="" />
+              </div>
+              <div class="country-data">
+                <h1>${item.name.common}</h1>
+                <div class="cols">
+                  <div class="col">
+                    <div class="row">
+                      <p class="key">Native Name:</p>
+                      <p class="value">${item.name.common}</p>
+                    </div>
+                    <div class="row">
+                      <p class="key">Population:</p>
+                      <p class="value">${item.population}</p>
+                    </div>
+                    <div class="row">
+                      <p class="key">Region:</p>
+                      <p class="value">${item.region}</p>
+                    </div>
+                    <div class="row">
+                      <p class="key">Time Zone:</p>
+                      <p class="value">${item.timezones[0]}</p>
+                    </div>
+                    <div class="row">
+                      <p class="key">Capital:</p>
+                      <p class="value">${item.capital}</p>
+                    </div>
+                  </div>
+                  <div class="col">
+                    <div class="row">
+                      <p class="key">Top Level Domain:</p>
+                      <p class="value">${item.tld[0]}</p>
+                    </div>
+                    <div class="row">
+                      <p class="key">Currencies:</p>
+                      <p class="value">${currencyKeys[0].name}, ${currencyKeys[0].symbol}</p>
+                    </div>
+                    <div class="row">
+                      <p class="key">Languages:</p>
+                      <p class="value">${languagesKeys[0]}</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="border-country">
+                  <h2>Border Countries</h2>
+                  <div class="countries">
+                    <button class="borders">France</button>
+                    <button class="borders">France</button>
+                    <button class="borders">France</button>
+                  </div>
+                </div>
+              </div>
+          `;
+        }
+      });
+    });
+  };
 });
